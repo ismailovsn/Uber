4f5a07ace32e2891668cffc18c73795b81c89072
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-06-24 21:02:20 +0900

    Initial Commit

---
+++ Uber/UberApp.swift
@@ -0,0 +1,17 @@
+//
+//  UberApp.swift
+//  Uber
+//
+//  Created by Саид-Насир Исмаилов on 2023/06/24.
+//
+
+import SwiftUI
+
+@main
+struct UberApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

37b5b3c52d818362d9330cd8ca6a76b8fc98f5a1
Said-Nasir Ismailov <ismailovsn12@gmail.com>
2023-06-29 07:02:32 +0900

    feat: add menu and search views

---
+++ Uber/Core/LocationSearch/Views/LocationSearchActivationView.swift
@@ -0,0 +1,36 @@
+//
+//  LocationSearchActivationView.swift
+//  Uber
+//
+//  Created by Саид-Насир Исмаилов on 2023/06/29.
+//
+
+import SwiftUI
+
+struct LocationSearchActivationView: View {
+    var body: some View {
+        HStack {
+            Rectangle()
+                .fill(Color.black)
+                .frame(width: 8, height: 8)
+                .padding(.horizontal)
+            
+            Text("Where to?")
+                .foregroundColor(Color(.darkGray))
+            
+            Spacer()
+        }
+        .frame(width: UIScreen.main.bounds.width - 64, height: 50)
+        .background(
+            Rectangle()
+                .fill(Color.white)
+                .shadow(color: .black, radius: 6)
+        )
+    }
+}
+
+struct LocationSearchActivationView_Previews: PreviewProvider {
+    static var previews: some View {
+        LocationSearchActivationView()
+    }
+}
